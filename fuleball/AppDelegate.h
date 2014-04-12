abfa36260c4360cc9f0fc322580483eade6cb486
Avenidanet <info@avenidanet.com>
2014-04-11 21:09:08 -0600

    first commit

---
+++ fuleball/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  fuleball
+//
+//  Created by Brian Salazar on 4/11/14.
+//  Copyright (c) 2014 Brian Salazar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ fuleballTests/fuleballTests.m
@@ -0,0 +1,34 @@
+//
+//  fuleballTests.m
+//  fuleballTests
+//
+//  Created by Brian Salazar on 4/11/14.
+//  Copyright (c) 2014 Brian Salazar. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface fuleballTests : XCTestCase
+
+@end
+
+@implementation fuleballTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
